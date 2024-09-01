08d3d5132fefc062999d8eb42a54dbb1ec22e510
tiyo5151 <s1F102400224@iniad.org>
2024-09-01 19:33:38 +0900

    治った

--- src/component/information.tsx
+++ src/component/information.tsx
@@ -1,11 +1,13 @@
-import useGame from '../hooks/useGame';
 import styles from '../pages/index.module.css';
 
-const Information: React.FC = () => {
-  const { winner } = useGame();
+interface Props {
+  winner: number | null;
+}
+
+const Information: React.FC<Props> = ({ winner }) => {
   return (
     <div className={styles.information}>
-      {winner !== null ? <h1>{winner}の勝利</h1> : <h1>対戦中...</h1>}
+      {winner !== null ? <h1>{winner}の勝利!</h1> : <h1>対戦中...</h1>}
     </div>
   );
 };
